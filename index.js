8dbdae2f59701e3187ecb3f49b08603bea2e9d4e
Michael Bryant <mbryant2025@gmail.com>
2022-05-04 21:49:52 -0400

    rick flash

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+window.onload=function(){
+    var myBtn = document.getElementById("myBtn")
+    myBtn.addEventListener("click", showRick);
+}
+
+
+
+function showRick() {
+
+    var img = document.createElement("img");
+    img.src = "rick.png";
+    var src = document.getElementById("header");
+
+    src.appendChild(img);
+
+    var delayInMilliseconds = 1000;
+
+    setTimeout(function() {
+        src.removeChild(img);
+    }, delayInMilliseconds);
+
+}
