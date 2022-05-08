0d6ef76916d6a38e20736cf2207524c7022580ad
Michael Bryant <mbryant2025@gmail.com>
2022-05-07 20:56:42 -0400

    transfer json with copy2

--- aircraft/collectAircraftData.py
+++ aircraft/collectAircraftData.py
@@ -1,3 +1,3 @@
 import shutil
 
-shutil.copyfile("/run/dump1090-fa/aircraft.json", "/var/www/html/PiWebsite/aircraft")
+shutil.copy2("/run/dump1090-fa/aircraft.json", "/var/www/html/PiWebsite/aircraft")
