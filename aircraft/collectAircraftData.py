b574d8297b0f552330bd18a6cd8897fdd20d3c68
Michael Bryant <mbryant2025@gmail.com>
2022-05-18 13:19:08 -0400

    inverted color scale

--- aircraft/collectAircraftData.py
+++ aircraft/collectAircraftData.py
@@ -84,7 +84,7 @@ while True:
         alt = plane_data['alt_geom'] if 'alt_geom' in plane_data else 0
 
         # <10000' is white, >40000' is dark blue
-        rg = 0 if alt < 10_000 else 255 if alt > 40_000 else alt / 40_000 * 255
+        rg = 255 if alt < 10_000 else 0 if alt > 40_000 else 1 - (alt / 40_000) * 255
 
         return [rg, rg, 255]
 
