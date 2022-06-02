db2f76e4c34ea2ebe2bad9fb7d29b077704b4d25
Michael Bryant <mbryant2025@gmail.com>
2022-06-01 21:53:28 -0400

    cube on index.html

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
 
-window.onload=function(){
-    var myBtn = document.getElementById("myBtn")
-    myBtn.addEventListener("click", showRick);
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0,2.5,2.5);
+camera.lookAt(new THREE.Vector3(0,0,0));
+
+const renderer = new THREE.WebGLRenderer( {
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setSize(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.005;
+    renderer.render(scene, camera);
 }
 
-
-
-function showRick() {
-
-    var img = document.createElement("img");
-    img.src = "rick.png";
-    var src = document.getElementById("header");
-
-    src.appendChild(img);
-
-    var delayInMilliseconds = 1000;
-
-    setTimeout(function() {
-        src.removeChild(img);
-    }, delayInMilliseconds);
-
-}
+animate();
