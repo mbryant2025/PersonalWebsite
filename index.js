e0281f7ca17e38fc34fa4670ef03ee071fd101f2
Michael Bryant <mbryant2025@gmail.com>
2022-06-02 20:33:40 -0400

    cube on side

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as THREE from '../libraries/three/build/three.module.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,2.5,2.5);
+camera.position.set(2,2,2);
 camera.lookAt(new THREE.Vector3(0,0,0));
 
 const renderer = new THREE.WebGLRenderer( {
@@ -16,16 +16,60 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
+
+const edges = new THREE.EdgesGeometry(geometry);
+const cube = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x00ff00 } ) );
+scene.add(cube);
+
+cube.rotation.x = -0.1;
+cube.rotation.y = -0.2;
+cube.rotation.z = 0.2;
+
+cube.position.x = 1.8;
+cube.position.y = 0;
+cube.position.z = -1.8;
+
 
 scene.add(cube);
 
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.y += 0.01;
-    cube.rotation.x += 0.005;
+
+var i = 0;
+
+function rotate() {
+    requestAnimationFrame(rotate);
+    cube.rotation.x += 0.004;
+    cube.rotation.y -= 0.0025;
+    cube.rotation.z -= 0.0001;
     renderer.render(scene, camera);
+
 }
 
-animate();
+rotate();
+
+function animate() {
+
+    setTimeout(function() {
+        i++
+        if (i < 100) {
+            cube.rotation.x += 0.0;
+            cube.rotation.y += 0.0;
+            renderer.render(scene, camera);
+            animate();
+        }
+
+
+    }, 100);
+
+    // requestAnimationFrame(animate);
+
+}
+
+function callAnimate() {
+    if (i < 99 && i > 0) {
+        return;
+    }
+    i = 0;
+    animate();
+}
+
+document.body.onscroll = callAnimate;
